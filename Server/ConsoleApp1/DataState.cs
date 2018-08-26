5c1f587f5a2a8f83f955186d5c267e113eaeb013
Bernardo Bellingrodt <bernardohbellingrodt@gmail.com>
2018-08-26 18:59:12 -0300

    First Commit

---
+++ Server/ConsoleApp1/DataState.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Sockets;
+
+namespace ConsoleApp1
+{
+    public class DataState
+    {
+        public const int BUFFER_SIZE = 1024;
+
+        public Socket handler = null;
+        public byte[] buffer = new byte[BUFFER_SIZE];
+
+        public DataState(Socket handler, byte[] buffer)
+        {
+            this.handler = handler;
+            this.buffer = buffer;
+        }
+    }
+}
